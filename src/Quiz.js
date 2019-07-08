31a5f00650c68b28527a67b308622513ba4bd69a
msingh-climax <maninder.singh@climaxmedia.com>
2019-07-08 15:12:44 -0400

    fix: div

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
     }
     render(){
         return (
-            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_test}</div></div>
+            <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
         )
     }
 }
